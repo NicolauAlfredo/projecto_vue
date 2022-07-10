f45d1365e5b330e6eaf1bf6a4f9a89aa787cf0c2
Nicolau Alfredo <nicolaualfredoalfredo@gmail.com>
2022-07-10 15:24:26 +0100

    Aprendendo as directivas (v-if, v-else e v-show)

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,18 +1,27 @@
 <template>
     <div>
-        <p>Estou a trabalhar no momento.</p>
-        <p>Utilizo as seguintes tecnologias: </p>
+        <p v-if ="esta_trabalhando"> Estou a trabalhar no momento.</p>
+        <p v-else> Estou em busca de novas oportunidades!</p>
+       <p>Utilizo as seguintes tecnologias: </p>
         <ul>
             <li>Java</li>
             <li>Python</li>
             <li>JavaScript</li>
             <li>PHP</li>
         </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'Info'
+        name: 'Info',
+        data() {
+            return {
+                email: "nicolaualfredo@yahoo.com",
+                esta_trabalhando: false,
+                mostrar_email: true
+            }
+        }
     }
 </script>
