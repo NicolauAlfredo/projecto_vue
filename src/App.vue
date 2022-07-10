f37d35684e045d05bd7ac316fa8589b6ade47b71
Nicolau Alfredo <nicolaualfredoalfredo@gmail.com>
2022-07-10 13:50:44 +0100

    Estudando componentes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <first/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import first from './components/first.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: "App",
+        components: {
+            first
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/first.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>Olá Vue!</h1>
+      <p>Meu nome é {{ nome }}, tenho {{ idade }} anos de idade e trabalho como {{ profissao }}.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'first',
+    data() {
+      return {
+        nome: "Nicolau",
+        idade: 23,
+        profissao: "Programador"
+      }
+    }
+  }
+</script>
